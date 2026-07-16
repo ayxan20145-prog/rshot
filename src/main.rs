244896c360773dc9690900b19a0ad162867e17a8
ayxan <ayxan.20145@gmail.com>
2026-07-16 20:30:42 +0400

    added selecting with slop

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,54 @@
 use anyhow::Result;
 use image::{ImageBuffer, Rgba};
+use std::{env, process::Command};
 use x11rb::{connection::Connection, protocol::xproto::ConnectionExt};
 
 fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
     let (conn, screen_num) = x11rb::connect(None)?;
 
     let screen = &conn.setup().roots[screen_num];
 
     let geometry = conn.get_geometry(screen.root)?.reply()?;
 
-    let width = geometry.width as u16;
-    let height = geometry.height as u16;
+    let (image, width, height) = match args.get(1).map(String::as_str) {
+        Some("--select") => {
+            let (x, y, width, height) = select_region()?;
 
-    let image = conn
-        .get_image(
-            x11rb::protocol::xproto::ImageFormat::Z_PIXMAP,
-            screen.root,
-            0,
-            0,
-            width,
-            height,
-            u32::MAX,
-        )?
-        .reply()?;
+            let image = conn
+                .get_image(
+                    x11rb::protocol::xproto::ImageFormat::Z_PIXMAP,
+                    screen.root,
+                    x,
+                    y,
+                    width,
+                    height,
+                    u32::MAX,
+                )?
+                .reply()?;
 
-    let width = width as u32;
-    let height = height as u32;
+            (image, width as u32, height as u32)
+        }
+        _ => {
+            let width = geometry.width as u16;
+            let height = geometry.height as u16;
+
+            let image = conn
+                .get_image(
+                    x11rb::protocol::xproto::ImageFormat::Z_PIXMAP,
+                    screen.root,
+                    0,
+                    0,
+                    width,
+                    height,
+                    u32::MAX,
+                )?
+                .reply()?;
+
+            (image, width as u32, height as u32)
+        }
+    };
 
     let buffer = image.data;
 
@@ -51,4 +74,18 @@ fn main() -> Result<()> {
     println!("saved screenshot.png");
 
     Ok(())
+}
+fn select_region() -> Result<(i16, i16, u16, u16)> {
+    let output = Command::new("slop").args(["-f", "%x %y %w %h"]).output()?;
+
+    let coords = String::from_utf8(output.stdout)?;
+
+    let mut values = coords.split_whitespace();
+
+    let x = values.next().unwrap().parse()?;
+    let y = values.next().unwrap().parse()?;
+    let w = values.next().unwrap().parse()?;
+    let h = values.next().unwrap().parse()?;
+
+    Ok((x, y, w, h))
 }
